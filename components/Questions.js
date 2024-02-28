73333e4f72e56378e25761fd4a4a41764d0206d6
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-02-28 14:55:57 +0200

    Answers now connected

--- components/Questions.js
+++ components/Questions.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-const quizUrl = 'https://opentdb.com/api.php?amount=5&type=multiple';
+const quizUrl = 'https://opentdb.com/api.php?amount=50&type=multiple';
 
 const Questions = () => {
     const [questionObjects, setQuestionObjects] = useState([]);
+    const [answers, setAnswers] = useState([]);
     const [selectedIndex, setSelectedIndex] = useState(0);
   
     const fetchQuestions = () => {
@@ -17,28 +18,74 @@ const Questions = () => {
           console.log('Error in fetch:', error);
         });
     };
-  
-    useEffect(() => {
-      fetchQuestions();
-    }, []);
-  
+
     const handleNextQuestion = () => {
-      setSelectedIndex(prevIndex => (prevIndex + 1) % questionObjects.length); // Cycle through questions
-    };
+        console.log('F: HANDLE NEXT QUESTION... \n'); // Corrected typo
+        if(questionObjects.length > 0){
+            setSelectedIndex(prevIndex => (prevIndex + 1) % questionObjects.length); // Cycle through questions
+        }
+        
+      };
+  
+
+    useEffect(() => {
+        console.log('F: FETCH QUESTIONS... \n');
+        fetchQuestions();
+      }, []);
+
+
+    //TODO join answers and shuffle them after questionsFetched
+    useEffect(() => {
+        console.log('F: JOINING ANSWERS... \n');
+
+
+        const tempAnswersArray = [];
+
+        questionObjects.map((question,index) => {
+
+            const tempAnswers = []
+
+            tempAnswers.push(question.correct_answer);
+
+            question.incorrect_answers.map((correctanswer, index) => {
+                tempAnswers.push(correctanswer);
+            });
+            
+            tempAnswersArray.push(tempAnswers)
+        });
+
+        
+
+        setAnswers(tempAnswersArray)
+
+        console.log('Answers useState:.... ', answers)
+
+
+
+    }, [questionObjects]);
+  
+
+
+
   
     return (
-      <SafeAreaView>
+        <SafeAreaView>
         {questionObjects.length > 0 && (
           <View>
-            <Text style={styles.question}>{questionObjects[selectedIndex].question}</Text>
-            <Text>Correct Answer: {questionObjects[selectedIndex].correct_answer}</Text>
-            <Text>Incorrect Answers: {questionObjects[selectedIndex].incorrect_answers.join(', ')}</Text>
+            <Text style={styles.question}>
+              {questionObjects[selectedIndex].question}
+            </Text>
+
+            {answers[selectedIndex].map((answer => (
+                <Text>{answer}</Text>
+            )))}
             <Button title="Next Question" onPress={handleNextQuestion} />
           </View>
         )}
       </SafeAreaView>
     );
-        }
+}
+
 export default Questions;
 
 const styles = StyleSheet.create({
