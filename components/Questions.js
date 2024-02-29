b9d2e7249133b8ed87bd67b3889297731005e112
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-02-29 15:41:21 +0200

    basic quiz works

--- components/Questions.js
+++ components/Questions.js
@@ -1,99 +1,106 @@
 import React, { useEffect, useState } from 'react';
 import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import he from 'he'; // Import the 'he' library
 
-const quizUrl = 'https://opentdb.com/api.php?amount=50&type=multiple';
+const quizUrl = 'https://opentdb.com/api.php?amount=10&type=multiple';
 
 const Questions = () => {
     const [questionObjects, setQuestionObjects] = useState([]);
     const [answers, setAnswers] = useState([]);
     const [selectedIndex, setSelectedIndex] = useState(0);
-  
+    const [selectedAnswer, setSelectedAnswer] = useState('');
+    const [points, setPoints] = useState(0);
+
     const fetchQuestions = () => {
-      fetch(quizUrl)
-        .then(res => res.json())
-        .then(json => {
-          setQuestionObjects(json.results); // Save all question objects
-        })
-        .catch(error => {
-          console.log('Error in fetch:', error);
-        });
+        fetch(quizUrl)
+            .then(res => res.json())
+            .then(json => {
+                setQuestionObjects(json.results); 
+            })
+            .catch(error => {
+                console.log('Error in fetch:', error);
+            });
+    };
+
+    const shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+
+    const handleAnswer = (ans) => {
+      console.log('answer check', ans, ' -- ', questionObjects[selectedIndex].correct_answer)
+
+        if (ans === questionObjects[selectedIndex].correct_answer) {
+            
+            setPoints(points + 1);
+        }
+        handleNextQuestion();
     };
 
     const handleNextQuestion = () => {
-        console.log('F: HANDLE NEXT QUESTION... \n'); // Corrected typo
-        if(questionObjects.length > 0){
-            setSelectedIndex(prevIndex => (prevIndex + 1) % questionObjects.length); // Cycle through questions
+        console.log('F: HANDLE NEXT QUESTION... \n');
+        if (questionObjects.length > 0) {
+            setSelectedIndex(prevIndex => (prevIndex + 1) % questionObjects.length); 
         }
-        
-      };
-  
+        setSelectedAnswer('');
+    };
 
     useEffect(() => {
         console.log('F: FETCH QUESTIONS... \n');
         fetchQuestions();
-      }, []);
+    }, []);
 
-
-    //TODO join answers and shuffle them after questionsFetched
     useEffect(() => {
         console.log('F: JOINING ANSWERS... \n');
-
-
         const tempAnswersArray = [];
 
-        questionObjects.map((question,index) => {
-
-            const tempAnswers = []
-
-            tempAnswers.push(question.correct_answer);
-
-            question.incorrect_answers.map((correctanswer, index) => {
-                tempAnswers.push(correctanswer);
+        if (questionObjects.length > 0) {
+            questionObjects.forEach(question => {
+                const answers = [question.correct_answer, ...question.incorrect_answers];
+                tempAnswersArray.push(shuffleArray(answers));
             });
-            
-            tempAnswersArray.push(tempAnswers)
-        });
+        }
 
-        
+        if (tempAnswersArray.length > 0) {
+            setAnswers(tempAnswersArray);
+        }
 
-        setAnswers(tempAnswersArray)
-
-        console.log('Answers useState:.... ', answers)
-
-
-
+        console.log('Answers useState:.... ', answers);
     }, [questionObjects]);
-  
 
-
-
-  
     return (
         <SafeAreaView>
-        {questionObjects.length > 0 && (
-          <View>
-            <Text style={styles.question}>
-              {questionObjects[selectedIndex].question}
-            </Text>
+            {answers.length > 0 && questionObjects.length > 0 && (
+                <View>
+                    <Text style={styles.question}>
+                        {he.decode(questionObjects[selectedIndex].question)}
+                    </Text>
+                    {answers[selectedIndex].map((answer, index) => (
+                        <Button 
+                            key={index} 
+                            title={he.decode(answer)}
+                            onPress={() => {handleAnswer(answer)}}
+                        />
+                    ))}
 
-            {answers[selectedIndex].map((answer => (
-                <Text>{answer}</Text>
-            )))}
-            <Button title="Next Question" onPress={handleNextQuestion} />
-          </View>
-        )}
-      </SafeAreaView>
+                    
+                    {/* <Button title="Submit Answer" onPress={handleAnswer} /> */}
+                    {/* <Button title="Next Question" onPress={handleNextQuestion} /> */}
+                    <Text>Points: {points}</Text>
+                </View>
+            )}
+        </SafeAreaView>
     );
 }
 
 export default Questions;
 
 const styles = StyleSheet.create({
-// Your styles here
-  question: {
-    padding: 10,
-    backgroundColor: 'lightblue',
-
-    // alignSelf:'center'
-  }
+    question: {
+        padding: 10,
+        backgroundColor: 'lightblue',
+    }
 });
